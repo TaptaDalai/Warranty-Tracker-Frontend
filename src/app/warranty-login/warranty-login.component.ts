7694dfc60b30230b1323c03f760b5ee2490e7e78
akhil9346 <perumandlaakhil2@gmail.com>
2024-06-18 14:24:24 +0530

    log in component added

--- src/app/warranty-login/warranty-login.component.ts
+++ src/app/warranty-login/warranty-login.component.ts
@@ -1,25 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-warranty-login',
+  imports:[ReactiveFormsModule,CommonModule],
+  standalone:false,
   templateUrl: './warranty-login.component.html',
   styleUrl: './warranty-login.component.css'
 })
 
 export class WarrantyLoginComponent implements OnInit {
-  loginForm!: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required, Validators.minLength(2)],
-      password: ['', [Validators.required, Validators.minLength(4)]],
-    });
+  userForm: FormGroup
+
+  constructor(){
+    this.userForm=new FormGroup({
+      userName: new FormControl("",[Validators.required]),
+      password: new FormControl("",[Validators.required,Validators.minLength(4)])
+      
+    })
+
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  }
+  // loginForm!: FormGroup;
+  // constructor(private formBuilder: FormBuilder) {}
+  // ngOnInit() {
+  //   this.loginForm = this.formBuilder.group({
+  //     username: ['', Validators.required, Validators.minLength(2)],
+  //     password: ['', [Validators.required, Validators.minLength(4)]],
+  //   });
     // this.loginForm.valueChanges.subscribe(data => console.log(data));
-  }
+  
+  
+  
+
+
  
-  submit() {
-    console.log(this.loginForm.value);
-  }
-}
+  // onsubmit() {
+  //   console.log(this.userForm.value);
+  // }
 
