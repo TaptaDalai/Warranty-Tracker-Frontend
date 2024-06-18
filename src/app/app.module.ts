9882e1af3da0b05c292d525715e6227fc9204436
Mourya <2282200@cognizant.com>
2024-06-18 15:42:54 +0530

    rrr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { WarrantyLoginComponent } from './warranty-login/warranty-login.component';
+
 import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { RegisterComponent } from './components/register/register.component';
