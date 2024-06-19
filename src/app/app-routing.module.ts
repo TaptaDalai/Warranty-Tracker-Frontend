71e07ae6d3166a7f24646fc75aec423346004727
akhil9346 <perumandlaakhil2@gmail.com>
2024-06-19 15:58:56 +0530

    to pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
+import { WarrantyLoginComponent } from './warranty-login/warranty-login.component';
 
 
 
-const routes: Routes = [
+const routes: Routes = [{path:"", redirectTo:"warranty-login", pathMatch:"full"},
+  {path:"warranty-login", component:WarrantyLoginComponent},
   {path: "register", component:RegisterComponent}
-
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { RegisterComponent } from './components/register/register.component';
+import { WarrantyLoginComponent } from './warranty-login/warranty-login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    WarrantyLoginComponent
   ],
   imports: [
     BrowserModule,
